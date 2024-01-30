2d2eb71aad977abd37a2562189bc82373292c1f8
tou13 <157106072+tou13@users.noreply.github.com>
2024-01-30 11:01:55 +0800

    Updated

--- linux/desktop/install-qq.sh
+++ linux/desktop/install-qq.sh
@@ -12,7 +12,29 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+target_user=${1:-ubuntu}
+target_user_home=$(getent passwd $target_user | cut -d: -f6)
+if [ -z "$target_user_home" ]; then
+    echo "用户 '$target_user' 不存在，跳过安装"
+    exit 1
+fi
+
 wget -O /tmp/$app_name.deb https://dldir1.qq.com/qqfile/qq/QQNT/feb78c41/linuxqq_3.2.5-21159_amd64.deb
 apt install /tmp/$app_name.deb
 
 echo "LinuxQQ安装成功"
+
+cat <<EOF > $target_user_home/Desktop/$app_name.desktop
+[Desktop Entry]
+Name=QQ
+Exec=/opt/QQ/qq --no-sandbox %U
+Terminal=false
+Type=Application
+Icon=/usr/share/icons/hicolor/512x512/apps/qq.png
+StartupWMClass=QQ
+Categories=Network;
+Comment=QQ
+MimeType=x-scheme-handler/tencent;
+EOF
+
+chmod +x "$target_user_home/Desktop/$app_name.desktop"
--- linux/desktop/install-wexin.sh
+++ linux/desktop/install-wexin.sh
@@ -12,7 +12,29 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+target_user=${1:-ubuntu}
+target_user_home=$(getent passwd $target_user | cut -d: -f6)
+if [ -z "$target_user_home" ]; then
+    echo "用户 '$target_user' 不存在，跳过安装"
+    exit 1
+fi
+
 wget -O /tmp/$app_name.deb http://archive.ubuntukylin.com/software/pool/partner/weixin_2.1.1_amd64.deb
 apt install /tmp/$app_name.deb
 
 echo "微信 安装成功"
+
+cat <<EOF > $target_user_home/Desktop/$app_name.desktop
+[Desktop Entry]
+Name[tr]=weixin
+Name[zh_CN]=微信
+Exec=/opt/weixin/weixin --no-sandbox %U
+Terminal=false
+Type=Application
+Icon=weixin
+StartupWMClass=weixin
+Comment=微信桌面版
+Categories=Utility;
+EOF
+
+chmod +x "$target_user_home/Desktop/$app_name.desktop"
