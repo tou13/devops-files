ada1a0b7d7bf1bdeb1bb89585d7299c574d85934
tou13 <157106072+tou13@users.noreply.github.com>
2024-02-22 05:31:11 +0000

    Updated

--- linux/install-hy2.sh
+++ linux/install-hy2.sh
@@ -8,6 +8,16 @@ fi
 port=${1:-443}
 password=$(openssl rand -hex 16)
 
+if ! command -v netstat &> /dev/null; then
+    echo "netstat 未安装，请先安装net-tools"
+    exit 1
+fi
+
+if netstat -tuln | grep -q ":$port\s"; then
+    echo "端口 $port 已被占用，请更换安装参数中的端口重新安装"
+    exit 1
+fi
+
 mkdir -p /etc/hysteria/
 
 openssl ecparam -genkey -name prime256v1 -out /etc/hysteria/private.key
@@ -37,7 +47,6 @@ bash <(curl -fsSL https://get.hy2.sh/)
 
 systemctl enable --now hysteria-server && \
 systemctl status hysteria-server
-
 
 echo "安装完成，password：$password，端口：$port，sni: www.bing.com。由于使用自签证书，请设置insecure为true。以下是clash配置示例："
 
