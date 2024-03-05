b9967dd380e9938aad9a3c33151c5789ae8cb9de
tou13 <157106072+tou13@users.noreply.github.com>
2024-03-05 05:24:45 +0000

    Updated

--- desktop/install-gitfiend.sh
+++ desktop/install-gitfiend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-app_name='obsidian'
+app_name='gitfiend'
 target_user=${1:-}
 
 output_message=$(bash <(curl -Ls https://raw.githubusercontent.com/tou13/somefiles/main/common/desktop-init-check.sh) $app_name $target_user)
@@ -9,22 +9,21 @@ if [ "$?" -ne 0 ]; then
     exit $?
 fi
 
-wget -O /tmp/$app_name.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/obsidian_1.5.3_amd64.deb
+wget -O /tmp/$app_name.deb https://gitfiend.com/resources/GitFiend_0.44.3_amd64.deb
 apt install /tmp/$app_name.deb
 
-echo "Obsidian 安装成功"
+echo "GitFiend 安装成功"
 
 cat <<EOF > $target_user_home/Desktop/$app_name.desktop
 [Desktop Entry]
-Name=Obsidian
-Exec=/opt/Obsidian/obsidian --no-sandbox %U
+Name=GitFiend
+Exec=/opt/GitFiend/gitfiend --no-sandbox %U
 Terminal=false
 Type=Application
-Icon=obsidian
-StartupWMClass=obsidian
-Comment=Obsidian
-MimeType=x-scheme-handler/obsidian;
-Categories=Office;
+Icon=gitfiend
+StartupWMClass=GitFiend
+Comment=A Git client designed for humans
+Categories=Development;
 EOF
 
 chmod +x "$target_user_home/Desktop/$app_name.desktop"
